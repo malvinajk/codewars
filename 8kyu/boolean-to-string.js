7adbaa7e199c53e78a7604dfec2c56a1c0c94891
Mal <malwina.janasik@gmail.com>
2024-11-01 18:16:42 +0000

    added more challenges

---
+++ 8kyu/boolean-to-string.js
@@ -0,0 +1,13 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+//     Note: Only valid inputs will be given.
+
+function booleanToString(b) {
+    //your code here
+    if (b) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
+}
